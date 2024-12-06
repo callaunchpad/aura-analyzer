d8573d835858cf66e90b2f4e567ad72198ca3ae2
Jade Wang <jadewang26@berkeley.edu>
2024-12-06 02:36:09 -0800

    :rocket: slight change to startup.sh to specify PORT

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 -m pip install -r requirements.txt
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT main:app
