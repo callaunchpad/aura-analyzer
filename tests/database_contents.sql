4dd32c776e53078dd38667176772901cac9e9456
Jade Wang <jadewang26@berkeley.edu>
2024-12-06 17:45:31 -0800

    :twisted_rightwards_arrows: merge with latest grailed db changes

---
+++ tests/database_contents.sql
@@ -0,0 +1,49 @@
+SELECT COUNT(*) FROM items WHERE ColorSeason="spring" AND MasterCategory="tops"; --217
+SELECT COUNT(*) FROM items WHERE ColorSeason="summer" AND MasterCategory="tops"; --132
+SELECT COUNT(*) FROM items WHERE ColorSeason="autumn" AND MasterCategory="tops"; --220
+SELECT COUNT(*) FROM items WHERE ColorSeason="winter" AND MasterCategory="tops"; --160
+
+SELECT COUNT(*) FROM items WHERE ColorSeason="spring" AND MasterCategory="bottoms"; --84
+SELECT COUNT(*) FROM items WHERE ColorSeason="summer" AND MasterCategory="bottoms"; --54
+SELECT COUNT(*) FROM items WHERE ColorSeason="autumn" AND MasterCategory="bottoms"; --72
+SELECT COUNT(*) FROM items WHERE ColorSeason="winter" AND MasterCategory="bottoms"; --54
+
+SELECT COUNT(*) FROM items WHERE ColorSeason="spring" AND MasterCategory="footwear"; --13
+SELECT COUNT(*) FROM items WHERE ColorSeason="summer" AND MasterCategory="footwear"; --7
+SELECT COUNT(*) FROM items WHERE ColorSeason="autumn" AND MasterCategory="footwear"; --22
+SELECT COUNT(*) FROM items WHERE ColorSeason="winter" AND MasterCategory="footwear"; --11
+
+SELECT COUNT(*) FROM items WHERE ColorSeason="spring" AND MasterCategory="outerwear"; --37
+SELECT COUNT(*) FROM items WHERE ColorSeason="summer" AND MasterCategory="outerwear"; --30
+SELECT COUNT(*) FROM items WHERE ColorSeason="autumn" AND MasterCategory="outerwear"; --51
+SELECT COUNT(*) FROM items WHERE ColorSeason="winter" AND MasterCategory="outerwear"; --35
+
+SELECT COUNT(*) FROM items WHERE ColorSeason="spring" AND MasterCategory="accessories"; --160
+SELECT COUNT(*) FROM items WHERE ColorSeason="summer" AND MasterCategory="accessories"; --87
+SELECT COUNT(*) FROM items WHERE ColorSeason="autumn" AND MasterCategory="accessories"; --137
+SELECT COUNT(*) FROM items WHERE ColorSeason="winter" AND MasterCategory="accessories"; --99
+
+SELECT COUNT(*) FROM items WHERE ColorSeason="spring" AND MasterCategory="womens_tops"; --231
+SELECT COUNT(*) FROM items WHERE ColorSeason="summer" AND MasterCategory="womens_tops"; --125
+SELECT COUNT(*) FROM items WHERE ColorSeason="autumn" AND MasterCategory="womens_tops"; --163
+SELECT COUNT(*) FROM items WHERE ColorSeason="winter" AND MasterCategory="womens_tops"; --133
+
+SELECT COUNT(*) FROM items WHERE ColorSeason="spring" AND MasterCategory="womens_bottoms"; --116 
+SELECT COUNT(*) FROM items WHERE ColorSeason="summer" AND MasterCategory="womens_bottoms"; --69
+SELECT COUNT(*) FROM items WHERE ColorSeason="autumn" AND MasterCategory="womens_bottoms"; --85
+SELECT COUNT(*) FROM items WHERE ColorSeason="winter" AND MasterCategory="womens_bottoms"; --57
+
+SELECT COUNT(*) FROM items WHERE ColorSeason="spring" AND MasterCategory="womens_footwear"; --56
+SELECT COUNT(*) FROM items WHERE ColorSeason="summer" AND MasterCategory="womens_footwear"; --66
+SELECT COUNT(*) FROM items WHERE ColorSeason="autumn" AND MasterCategory="womens_footwear"; --43
+SELECT COUNT(*) FROM items WHERE ColorSeason="winter" AND MasterCategory="womens_footwear"; --39
+
+SELECT COUNT(*) FROM items WHERE ColorSeason="spring" AND MasterCategory="womens_outerwear"; --81
+SELECT COUNT(*) FROM items WHERE ColorSeason="summer" AND MasterCategory="womens_outerwear"; --63
+SELECT COUNT(*) FROM items WHERE ColorSeason="autumn" AND MasterCategory="womens_outerwear"; --56
+SELECT COUNT(*) FROM items WHERE ColorSeason="winter" AND MasterCategory="womens_outerwear"; --41
+
+SELECT COUNT(*) FROM items WHERE ColorSeason="spring" AND MasterCategory="womens_accessories"; --81
+SELECT COUNT(*) FROM items WHERE ColorSeason="summer" AND MasterCategory="womens_accessories"; --66
+SELECT COUNT(*) FROM items WHERE ColorSeason="autumn" AND MasterCategory="womens_accessories"; --67
+SELECT COUNT(*) FROM items WHERE ColorSeason="winter" AND MasterCategory="womens_accessories"; --38
