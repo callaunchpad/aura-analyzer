53e60e5aeb44ad62f8e05631a4141a7baa35b852
Anaisha Das <anaishadas@gmail.com>
2024-12-07 02:26:52 -0800

    handled run_demo error case

--- run_demo.sh
+++ run_demo.sh
@@ -9,6 +9,20 @@ fi
 # Assign the argument to a variable
 IMAGE_FILE=$1
 
+
+#Check if  file exists
+if [ ! -f "combined_demo/input-imgs/$IMAGE_FILE" ]; then
+    echo "Error: File '$IMAGE_FILE' does not exist."
+    exit 1
+fi
+
+
+#Checks if file is an image
+if ! file "combined_demo/input-imgs/$IMAGE_FILE" | grep -iE 'image|bitmap'; then
+    echo "Error: '$image_file' is not a valid image file."
+    exit 1
+fi
+
 # Run the Python script with the JPEG file
 python3 color_correction/PyTorch/demo_single_image.py --i "combined_demo/input-imgs/$IMAGE_FILE" -t AWB -o combined_demo/intermediate-imgs
 
